181ae28c221441193ff6868df4d92bcd37f811dd
Mateus <mateussab1@gmail.com>
2026-04-30 20:45:57 -0300

    adicionado método que salva dados usuário

---
+++ src/main/java/com/mateus/usuario/controller/UsuarioController.java
@@ -0,0 +1,25 @@
+package com.mateus.usuario.controller;
+
+import com.mateus.usuario.business.UsuarioService;
+import com.mateus.usuario.business.dto.UsuarioDTO;
+import com.mateus.usuario.infrastructure.entity.Usuario;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/usuario")
+@RequiredArgsConstructor
+public class UsuarioController {
+
+    private final UsuarioService usuarioService;
+
+    @PostMapping
+    public ResponseEntity<UsuarioDTO> salvaUsuario(@RequestBody UsuarioDTO usuarioDTO) {
+        return ResponseEntity.ok(usuarioService.salvaUsuario(usuarioDTO));
+    }
+
+}
